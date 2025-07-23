edb9d4a7fb8599c84add62c122e29ee9219f3d4e
Archi <Thakkararchi3@gmail.com>
2025-07-24 00:06:41 +0530

    Added Starter Template

---
+++ src/test/java/com/crownstonebank/CrownstoneBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crownstonebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrownstoneBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
